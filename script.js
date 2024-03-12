bb37bba188e93d88be29a2571e2d09b94a50a849
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-03-12 13:47:38 +0200

    Initial commit script.js file

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+//This code Validates the Input 
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
+function validateInput(input) {
+    if (/^pet_\d{4}/.test(input)) {
+        return "Input Is Valid 🟢";
+    } else {
+        return "Input is Invalid 🔴";
+    }
 }
 
+function validateSyntax() {
+    //We got the id of the input so that we know what has been written inside of it.
+    let input = document.getElementById('petInput').value;
+
+    //i created a variable for my
+    const validatedOutput = validateInput(input); //created a variable to contain or store the funtion for testing if our input is correct
+
+    //Now we use
+    const result = document.getElementById('result');
+
+    result.textContent = validatedOutput;
+
+
+}
 
