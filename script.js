f46097fa6dcb7529063fa48d9e09d4926af4174f
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-03-12 14:30:09 +0200

    Shorten the code and made a comment for the change i made

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//This code Validates the Input 
+//This code Validates the Input First , then we are going to add an argument to it later on when we check what have been written inside an inputfield.
 
 function validateInput(input) {
-    if (/^pet_\d{4}/.test(input)) {
+    // 
+    if (/^pet_\d{4}[a-zA-Z]/.test(input)) {
         return "Input Is Valid 🟢";
     } else {
         return "Input is Invalid 🔴";
@@ -10,15 +11,14 @@ function validateInput(input) {
 
 function validateSyntax() {
     //We got the id of the input so that we know what has been written inside of it.
-    let input = document.getElementById('petInput').value;
+    let input = document.getElementById('petInput').value; //we need to put this var inside a validateInput. So that it can see what has been written and what to validate (simply its our argument).
 
-    //i created a variable for my
-    const validatedOutput = validateInput(input); //created a variable to contain or store the funtion for testing if our input is correct
+    // const validatedOutput = validateInput(input); //created a variable to contain or store the funtion for testing if our input is correct.
 
-    //Now we use
+    //we are getting the results id so that we can call it to display our output 
     const result = document.getElementById('result');
 
-    result.textContent = validatedOutput;
+    result.textContent = validateInput(input); //this 'input' is from the real Input value.
 
 
 }
